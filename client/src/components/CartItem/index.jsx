f50fe82e36dd2b084be2e83daf1fbba9105793f6
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-06-27 22:07:52 -0400

    add offline functionality by using indexDB to store data

--- client/src/components/CartItem/index.jsx
+++ client/src/components/CartItem/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
@@ -10,6 +11,7 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id,
     });
+    idbPromise('cart', 'delete', { ...item });
   };
 
   const onChange = (e) => {
@@ -20,12 +22,16 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id,
       });
+
+      idbPromise('cart', 'delete', { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value),
       });
+
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
     }
   };
 
@@ -40,7 +46,12 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} onChange={onChange} />
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
           <span
             role="img"
             aria-label="trash"
