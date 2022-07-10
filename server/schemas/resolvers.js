c954a9986f7887256d1a1f2f36e4d9dd8f0801b6
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-07-10 16:45:15 -0400

    implement stripe for React

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -66,7 +66,7 @@ const resolvers = {
         const product = await stripe.products.create({
           name: products[i].name,
           description: products[i].description,
-          images: [`${url}/images/${products[i].image}`] 
+          images: [`${url}/images/${products[i].image}`],
         });
 
         // generate price id using the product id
@@ -87,8 +87,7 @@ const resolvers = {
         payment_method_types: ['card'],
         line_items,
         mode: 'payment',
-        success_url:
-          'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`,
       });
 
